2b30cb2d1ddc92b4f89d104017eb570f473a89d7
Artem Gafarov <a.m.gafarov@ya.ru>
2024-11-19 13:08:53 +0300

    Refactoring

--- internal/config/yandexart/yandexArtConfig.go
+++ internal/config/yandexart/yandexArtConfig.go
@@ -8,11 +8,11 @@ import (
 const (
 	GENERATION_URL = "https://llm.api.cloud.yandex.net/foundationModels/v1/imageGenerationAsync"
 	CHECK_URL      = "https://llm.api.cloud.yandex.net:443/operations/"
-	MODEL_URI      = "art://b1gjtlqofdt5mu5io6a9/yandex-art/latest"
 	DELAY_SEC      = 3 * time.Second
 	WAIT_TIMEOUT   = 1 * time.Minute
 )
 
 var (
-	API_KEY = os.Getenv("API_KEY")
+	API_KEY   = os.Getenv("API_KEY")
+	MODEL_URI = "art://" + os.Getenv("STORAGE_ID") + "/yandex-art/latest"
 )
